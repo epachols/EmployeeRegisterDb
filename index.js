88c15276259c04d0649c3e3981ff86ef25191fb0
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-07-13 16:19:41 -0700

    functional scheme, then adding seeds to play with data more readily

---
+++ index.js
@@ -0,0 +1,20 @@
+const inq = require("inquirer");
+const mysql = require("mysql");
+//TODO: may need to set PORT to process.env.port || 3306
+//TODO: may need to set PORT to process.env.port || 3306
+var connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "password",
+    database: "employee_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    //init()
+})
